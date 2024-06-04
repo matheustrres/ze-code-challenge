0a3c33549ddbe2c7b838bdb528c52c2998241e17
Matheus Torres <torresmiranda.rj@gmail.com>
2024-06-04 18:08:20 -0300

    test: ensure SUT throws if coverage area coordinates is not an array

--- tests/domain/entities/value-objects/coverage-area.spec.ts
+++ tests/domain/entities/value-objects/coverage-area.spec.ts
@@ -7,7 +7,7 @@ import { CoverageArea } from '@/domain/entities/value-objects/coverage-area';
 import { CoverageAreaError } from '@/domain/errors/coverage-area';
 
 describe('CoverageArea value object', () => {
-	it('should throw if coverage area type is not MultiPolygon', () => {
+	it('should throw if given coverage area type is not MultiPolygon', () => {
 		const invalidCoverageArea = {
 			type: 'Point',
 			coordinates: [
@@ -26,6 +26,20 @@ describe('CoverageArea value object', () => {
 		throws(
 			() => CoverageArea.create(invalidCoverageArea as MultiPolygon),
 			new CoverageAreaError('Argument {type} must be "MultiPolygon".'),
+		);
+	});
+
+	it('should throw if given coverage area coordinates is not an array', () => {
+		const invalidCoverageArea = {
+			type: 'MultiPolygon',
+			coordinates: {},
+		};
+
+		throws(
+			() => CoverageArea.create(invalidCoverageArea as MultiPolygon),
+			new CoverageAreaError(
+				'Argument {coordinates} must be an array of numbers.',
+			),
 		);
 	});
 });
