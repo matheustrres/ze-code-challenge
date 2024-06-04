0a86f5595904c7c781623c0537cf98dc9d15a1b4
Matheus Torres <torresmiranda.rj@gmail.com>
2024-06-04 19:36:05 -0300

    test: ensure SUT returns true if a valid cpf value is provided

--- tests/libs/brazilian-utils.spec.ts
+++ tests/libs/brazilian-utils.spec.ts
@@ -11,5 +11,12 @@ describe('BrazilianUtils lib', () => {
 			deepStrictEqual(BrazilianUtils.isValidCPF('abcdefghtia'), false);
 			deepStrictEqual(BrazilianUtils.isValidCPF('123456'), false);
 		});
+
+		it('should return true if a valid value is provided', () => {
+			deepStrictEqual(BrazilianUtils.isValidCPF('403.64478-829'), true);
+			deepStrictEqual(BrazilianUtils.isValidCPF('256.657.510-06'), true);
+			deepStrictEqual(BrazilianUtils.isValidCPF('105.068.620-97'), true);
+			deepStrictEqual(BrazilianUtils.isValidCPF('93867339007'), true);
+		});
 	});
 });
